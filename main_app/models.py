b002a7fa6477eafdf67a07b1749c3b8c5d0f642e
Tay West <tayanne.west@gmail.com>
2021-11-25 10:49:49 -0500

    add signup

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db.models.fields import BooleanField, TextField
 
 # Create your models here.
+
+MEDIATYPE = (
+  ("CH", "Charcoal"),
+  ("CP", "Colored Pencil"),
+  ("IA", "Ink: Alcohol-based"),
+  ("II", "Ink: India"),
+  ("IW", "Ink: Water-based"),
+  ("MA", "Marker: Alcohol-based"),
+  ("MW", "Marker: Water-based"),
+  ("PA", "Paint: Acrylic"),
+  ("PG", "Paint: Gouache"),
+  ("PO", "Paint: Oil"),
+  ("PW", "Paint: Watercolor"),
+  ("PH", "Pastel: Hard"),
+  ("OP", "Pastel: Oil"),
+  ("PP", "Pastel: Pan"),
+  ("PS", "Pastel: Soft "),
+  ("PB", "Pen: Brush nib"),
+  ("PL", "Pen: Fineliner"),
+  ("PF", "Pen: Fountain"),
+)
+
+class Artsupply(models.Model):
+  name = models.CharField(max_length = 100)
+  brand = models.CharField(max_length = 100)
+  media = models.CharField(
+    max_length=2,
+    choices=MEDIATYPE,
+    default=MEDIATYPE[0][0])
+  familiarity = models.IntegerField(
+    default=0,
+    validators=[MaxValueValidator(5), MinValueValidator(0)]
+  )
+  description = TextField(max_length=500)
+  favorite = BooleanField()
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
--- main_app/views.py
+++ main_app/views.py
@@ -1,25 +1,44 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from .models import Artsupply
+
 
 # Create your views here.
-def home(request):
-  return HttpResponse("<h1>Cool stuff will go here later on GOD on god</h1>")
+class Home(LoginView):
+  template_name = "home.html"
 
 def about(request):
   return render(request, "about.html")
 
-class Artsupply:
-  def __init__(self, name, brand, media, familiarity, description, favorite):
-    self.name = name
-    self.brand = brand
-    self.media = media
-    self.familiarity = familiarity
-    self.description = description
-    self.favorite = favorite
+# class Artsupply:
+#   def __init__(self, name, brand, media, familiarity, description, favorite):
+#     self.name = name
+#     self.brand = brand
+#     self.media = media
+#     self.familiarity = familiarity
+#     self.description = description
+#     self.favorite = favorite
 
-artsupplies = [
-  Artsupply("Pigma Micron", "Sakura", "Fineliner", 5, "So good", "true"),
-]
+# artsupplies = [
+#   Artsupply("Pigma Micron", "Sakura", "Fineliner", 5, "So good", "true"),
+# ]
 
 def artsupplies_index(request):
+  artsupplies = Artsupply.objects.all()
   return render(request, "artsupplies/index.html", {"artsupplies": artsupplies})
+
+def signup(request):
+  error_message = ""
+  if request.method == "POST":
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect("artsupplies_index")
+    else:
+      error_message = "Invalid sign up - please try again"
+  form = UserCreationForm()
+  context = {"form": form, "error_message": error_message}
+  return render(request, "signup.html", context)
